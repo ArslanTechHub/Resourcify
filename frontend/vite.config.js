88f5cd8c36ffbf2212930d169b001b90c803185b
Arsalan <arsalan.online24@gmail.com>
2025-05-13 20:08:39 +0500

    updated 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     port:3000,
+//     host: '0.0.0.0', 
+//   }
+//   })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    host: '0.0.0.0', 
-  }
-  })
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  build: {
+    target: 'esnext', // modern build targeting the latest JavaScript
+    minify: 'esbuild', // Using esbuild for faster minification
+    outDir: 'dist', // Set output directory for production build
+  },
+})
