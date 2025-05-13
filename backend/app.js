e728d3acec74166f5b6031f8ab77f9e4daeb1e76
Arsalan <arsalan.online24@gmail.com>
2025-05-13 20:55:40 +0500

    update 8

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,13 @@ const app = express();
 config({
   path: "./config/config.env",
 });
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use((req, res, next) => {//https://yanfeng-client.onrender.com
+  res.setHeader('Access-Control-Allow-Origin', 'https://resourcify-1.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(express.json());
 app.use(
