0e765cff22c4df700fcf6e91842a422fe16652a1
Arsalan <arsalan.online24@gmail.com>
2025-05-13 20:41:59 +0500

    updated 6

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ const app = express();
 config({
   path: "./config/config.env",
 });
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(
