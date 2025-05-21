7f129b605c0d11421181b076117ed71d540795e5
muhammad-zeeshan-zia <zeeshanzia1270@gmail.com>
2025-05-21 05:11:52 +0500

    fixed links

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ import { ErrorMiddleware } from "./middlewares/Error.js";
 const app = express();
 
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://resourcify-chi.vercel.app');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://resourcify-chi.vercel.app');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
